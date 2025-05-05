0ccd219a3796d3446a0e6fd58164c0e13e47bac6
Joaquin Gomez Muñoz <96019@sistemas.frc.utn.edu.ar>
2025-05-05 11:36:11 -0300

    correccion de inicializacion de express

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,9 @@
-import { express } from 'express';
-
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
-
-app.use(express.static("../frontend"));
-
+app.use(express.static("frontend"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
