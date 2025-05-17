616927bf45f5d63cf237ffa028551d40b13d8003
Joaquin Gomez Muñoz <96019@sistemas.frc.utn.edu.ar>
2025-05-17 20:18:15 -0300

    eliminación de la carpeta JS

--- JS/backend/entities/sesion.js
+++
@@ -1,7 +0,0 @@
-class Sesion {
-  constructor(usuario, inicio, fin) {
-    this.usuario = usuario;
-    this.fechaHoraInicio = inicio;  // new Date() es fecha y hora actual
-    this.fechaHoraFin = fin;
-  }
-}
--- JS/backend/entities/usuario.js
+++
@@ -1,7 +0,0 @@
-class Usuario {
-    constructor(empleado, nombre, contrasena) {
-        this.empleado = empleado;
-        this.nombre = nombre; 
-        this.contrasena = contrasena; 
-    }
-}
