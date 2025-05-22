c02e8a5d03846d09d54fcd3f23effbd0ca9a1093
Joaquin Gomez Muñoz <96019@sistemas.frc.utn.edu.ar>
2025-05-22 13:04:14 -0300

    getNombre()

--- entidades/estado.py
+++ entidades/estado.py
@@ -3,6 +3,9 @@ class Estado:
         self.ambito = ambito
         self.nombre = nombre
 
+    def getNombre(self):
+        return self.nombre
+    
     def estaRealizada(self):
         return self.nombre == "Completamente Realizada"
     
